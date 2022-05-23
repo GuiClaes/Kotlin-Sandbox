ed0a9ef4e29f8187f043314359aef914e754401c
Guillaume <guillaumeclaes@yahoo.fr>
2022-05-23 22:21:00 +0200

    Number type lesson

--- src/main/kotlin/FirstFile.kt
+++ src/main/kotlin/FirstFile.kt
@@ -1,18 +1,18 @@
+import java.time.LocalDate
+
 const val AUTHOR = "GuiClaes"
 
 fun main(args: Array<String>) {
     println("Author : $AUTHOR")
     printRabbit()
     printTotalPrice("Mary", 3, 30)
-
-
-
+    //handleInput()
 }
 
 fun printRabbit(){
-    val ears: String = "(\\(\\"
-    val face: String = "(-.-)"
-    val body: String = "o_(\")(\")"
+    val ears = "(\\(\\"
+    val face = "(-.-)"
+    val body = "o_(\")(\")"
 
     println(ears);
     println(face);
@@ -21,4 +21,18 @@ fun printRabbit(){
 
 fun printTotalPrice(client:String, nbProducts:Int, price:Int) {
     println("$client want to buy $nbProducts which cost $price per unit. This will cost her ${price*nbProducts} bucks.")
+}
+
+fun handleInput() {
+    print("Enter your name: ")
+    val username = readLine()?:""
+    println("Hello $username!")
+
+    print("Number = ")
+    val result = readLine()?:""
+    println("5 * $result = ${result.toInt()*5}")
+
+    print("Enter your birth year: ")
+    val birthYear = readLine()?:""
+    println("You age is about ${LocalDate.now().year - birthYear.toInt()} years old")
 }
---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,42 @@
+const val pi = 3.14159
+
+fun main(args: Array<String>) {
+    showPIInfo()
+    showNumberType("15")
+    //doMath()
+}
+
+fun showPIInfo() {
+    println(pi::class.java)
+    println(pi.toFloat())
+    println(pi.toInt())
+    println(pi.toString())
+}
+
+fun showNumberType(value :String) {
+    val intValue = value.toInt()
+    println(intValue::class.java)
+
+    val byteValue = value.toByte()
+    println(byteValue::class.java)
+
+    val shortValue = value.toShort()
+    println(shortValue::class.java)
+
+    val longValue = value.toLong()
+    println(longValue::class.java)
+
+    val floatValue = value.toFloat()
+    println(floatValue::class.java)
+
+    val doubleValue = value.toDouble()
+    println(doubleValue::class.java)
+}
+
+fun doMath() {
+    print("Enter a double: ")
+    val doubleValue = readLine() ?: "0.0"
+    val result = doubleValue.toDouble() * pi;
+    println(result::class.java)
+    println(result)
+}
